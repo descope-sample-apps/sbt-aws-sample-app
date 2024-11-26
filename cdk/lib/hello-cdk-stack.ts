1deb8f55c710b623c339d4c4b383b7d3730a1982
Kevin J Gao <32936811+gaokevin1@users.noreply.github.com>
2024-11-26 13:29:28 -0800

    added cors headers

--- cdk/lib/hello-cdk-stack.ts
+++ cdk/lib/hello-cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as sbt from "@cdklabs/sbt-aws";
+import * as apigateway from "aws-cdk-lib/aws-apigatewayv2";
 import { Stack } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { DescopeAuth } from "sbt-aws-descope";
@@ -17,6 +18,18 @@ export class ControlPlaneStack extends Stack {
     const controlPlane = new sbt.ControlPlane(this, "ControlPlane", {
       auth: descopeAuth,
       systemAdminEmail: "support@descope.com",
+      apiCorsConfig: {
+        allowOrigins: ["https://sbt-aws-sample-app.preview.descope.org"],
+        allowMethods: [apigateway.CorsHttpMethod.ANY],
+        allowHeaders: [
+          "Content-Type",
+          "Authorization",
+          "X-Amz-Date",
+          "X-Api-Key",
+          "X-Amz-Security-Token",
+        ],
+        allowCredentials: true,
+      },
     });
 
     this.eventManager = controlPlane.eventManager;
